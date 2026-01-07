8c462054864995560b0ef268fb44edcd3b19db09
phanendra695-crypto <phanendra695@gmail.com>
2026-01-07 11:50:46 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,29 +1,13 @@
 import { db } from "./firebase.js";
-import {
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, addDoc }
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const inputs = document.querySelectorAll("input");
-const button = document.querySelector("button");
-
-button.addEventListener("click", async () => {
-  const name = inputs[0].value;
-  const entryFee = parseInt(inputs[1].value);
-  const time = inputs[2].value;
-
-  if (!name || !entryFee || !time) {
-    alert("Fill all fields");
-    return;
-  }
-
+document.querySelector("button").onclick = async () => {
+  const i = document.querySelectorAll("input");
   await addDoc(collection(db, "tournaments"), {
-    name,
-    entryFee,
-    time,
-    createdAt: new Date()
+    name: i[0].value,
+    fee: i[1].value,
+    time: i[2].value
   });
-
-  alert("Tournament created");
-});
-
+  alert("Tournament Created");
+};
