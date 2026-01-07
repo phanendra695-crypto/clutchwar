c9a5a687a1d315d9c6d47d5b329da694a354cb40
phanendra695-crypto <phanendra695@gmail.com>
2026-01-07 11:31:04 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,55 @@
 
+import { auth, db } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// REGISTER
+const registerForm = document.getElementById("registerForm");
+if (registerForm) {
+  registerForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("regEmail").value;
+    const password = document.getElementById("regPassword").value;
+
+    try {
+      const userCred = await createUserWithEmailAndPassword(auth, email, password);
+
+      await setDoc(doc(db, "users", userCred.user.uid), {
+        email: email,
+        coins: 0,
+        createdAt: new Date()
+      });
+
+      alert("Registration successful");
+      window.location.href = "login.html";
+    } catch (err) {
+      alert(err.message);
+    }
+  });
+}
+
+// LOGIN
+const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("loginEmail").value;
+    const password = document.getElementById("loginPassword").value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "wallet.html";
+    } catch (err) {
+      alert(err.message);
+    }
+  });
+}
